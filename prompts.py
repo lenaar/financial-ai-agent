7b099752cd35e4dca9c9e0b7e61071c2b262ba6c
Elena <rahimova.elena@gmail.com>
2025-06-16 17:30:47 +0200

    feat: add message to analyze data

--- prompts.py
+++ prompts.py
@@ -1,4 +1,6 @@
-GATHER_FINANCIAL_PROMPT = """You are a financial analyst. You are given a list of companies and you need to gather the financial data for each company."""
+GATHER_FINANCIAL_PROMPT = """You are a financial analyst. You are given a finacials for 5 years of a company and you need to gather the financial data for a company."""
+ANALYZE_DATA_PROMPT = """You are a financial analyst. You are given a list of companies and you need to analyze the financial data for a company."""
+
 
 
 
