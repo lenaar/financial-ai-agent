1b775ec97a7bd4f03fa2f5e48a8944d03c53b64f
Elena <rahimova.elena@gmail.com>
2025-06-16 19:26:47 +0200

    feat: add compare performance prompt draft

--- prompts.py
+++ prompts.py
@@ -1,9 +1,12 @@
 GATHER_FINANCIAL_PROMPT = """You are a financial analyst. You are given a finacials for 5 years of a company and you need to gather the financial data for a company."""
-ANALYZE_DATA_PROMPT = """You are a financial analyst. You are given a list of companies and you need to analyze the financial data for a company."""
+ANALYZE_DATA_PROMPT = """You are a financial analyst. You are given a financials of a company and you need to analyze the financial data for a company."""
 ANALYZE_COMPETITORS_PROMPT = """You are a financial analyst. 
-    You are given a list of companies and you need to research the competitors for a company for performance comparison. 
+    You are given a company and you need to research the competitors for performance comparison. 
     Provide max 3 queries for each competitor.
     Return the queries in a list of strings."""
+COMPARE_PERFORMANCE_PROMPT = """You are a financial analyst. You are given a list of companies and you need to compare the performance of the companies.
+    ***MAKE SURE TO COMPARE THE COMPETITORS PERFORMANCE TO THE COMPANY YOU ARE ANALYZING AND INCLUDE THE NAME OF COMPETITORS IN THE REPORT***"""
+
 
 
 
