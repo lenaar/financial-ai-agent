ee615e0b9f484b7a3de5d9b87abb5281ac0e8764
Elena <rahimova.elena@gmail.com>
2025-06-16 17:44:52 +0200

    fix: read from file directly

--- csv_data.py
+++ csv_data.py
@@ -1,5 +1,4 @@
 from state import State
-from io import StringIO
 import pandas as pd
 from langchain_core.messages import SystemMessage, HumanMessage
 from model import model
@@ -9,12 +8,14 @@ def get_csv_data(state: State) -> str:
     Gather the financial data for the company from a csv file.
     """
     csv_file = state["csv_file"]
-    data = pd.read_csv(StringIO(csv_file))
+    data = pd.read_csv(csv_file)
     
     data_str = data.to_string(index=False)
 
     content = (
         f"Here is the financial data for the chosen company: {data_str}"
     )
+
+    print(f"CSV content: {content}")
 
     return content
