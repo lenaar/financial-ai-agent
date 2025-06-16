427098adda43c0fb68545bab91d8fd1ed28d6fd1
Elena <rahimova.elena@gmail.com>
2025-06-16 17:45:33 +0200

    feat: add new prompt for competitors analysis

--- prompts.py
+++ prompts.py
@@ -1,7 +1,9 @@
 GATHER_FINANCIAL_PROMPT = """You are a financial analyst. You are given a finacials for 5 years of a company and you need to gather the financial data for a company."""
 ANALYZE_DATA_PROMPT = """You are a financial analyst. You are given a list of companies and you need to analyze the financial data for a company."""
-
-
+ANALYZE_COMPETITORS_PROMPT = """You are a financial analyst. 
+    You are given a list of companies and you need to research the competitors for a company for performance comparison. 
+    Provide max 3 queries for each competitor.
+    Return the queries in a list of strings."""
 
 
 
