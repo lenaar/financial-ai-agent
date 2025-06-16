f8a1e7378284a4a4605260defd63300aa42a1bae
Elena <rahimova.elena@gmail.com>
2025-06-16 18:52:58 +0200

    refactor: move general model response to model.py

--- model.py
+++ model.py
@@ -1,9 +1,21 @@
 import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage
 
 load_dotenv()
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 model = ChatOpenAI(api_key=openai_api_key, model="gpt-4.1-nano", temperature=0.0)
+
+
+def generate_model_response(content: str, prompt: str) -> str:
+    messages = [
+        SystemMessage(content=prompt),
+        HumanMessage(content=content)
+    ]
+
+    data = model.invoke(messages)
+
+    return data.content
