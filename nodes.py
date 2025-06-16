5557a48acd54081d164774343c1c4393178b0617
Elena <rahimova.elena@gmail.com>
2025-06-16 17:46:08 +0200

    feat: add analysis of competitors loop

--- nodes.py
+++ nodes.py
@@ -1,4 +1,4 @@
-from prompts import GATHER_FINANCIAL_PROMPT, ANALYZE_DATA_PROMPT, RESEARCH_COMPETITORS_PROMPT
+from prompts import GATHER_FINANCIAL_PROMPT, ANALYZE_DATA_PROMPT, ANALYZE_COMPETITORS_PROMPT
 from langchain_core.messages import SystemMessage, HumanMessage
 from model import model
 from typing import Dict, List
@@ -15,4 +15,21 @@ def message_node(content: str, prompt: str) -> str:
 
     return data.content
 
+
+def analyze_competitors_node(content: str, competitors: List[str]) -> str:
+    for competitor in competitors:
+        queries = model.with_structured_output(Queries).invoke(
+            [
+                SystemMessage(content=ANALYZE_COMPETITORS_PROMPT),
+                HumanMessage(content=competitor)
+            ]
+        )
+    
+    for query in queries.queries:
+        research_content = tavily.search(query=query, max_results=2)
+        for result in research_content["results"]:
+            content.append(result["content"])
+
+
+    return content
 
