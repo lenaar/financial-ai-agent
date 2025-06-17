dc81024ef25e830042a23ed51851877b947fdc81
Elena <rahimova.elena@gmail.com>
2025-06-17 17:19:21 +0200

    feat: read csv data as string io to prepare for streamlit app

--- csv_data.py
+++ csv_data.py
@@ -2,13 +2,14 @@ from state import State
 import pandas as pd
 from langchain_core.messages import SystemMessage, HumanMessage
 from model import model
+from io import StringIO
 
 def get_csv_data(state: State) -> str:
     """
     Gather the financial data for the company from a csv file.
     """
-    csv_file = state["csv_file"]
-    data = pd.read_csv(csv_file)
+    csv_data = state["csv_data"]
+    data = pd.read_csv(StringIO(csv_data))
     
     data_str = data.to_string(index=False)
 
