2f6dc090927e431183eeb996e3bc45dfd747ade7
Elena <rahimova.elena@gmail.com>
2025-06-17 15:02:00 +0200

    refactor: change state to have a general content for competitors and other informations

--- state.py
+++ state.py
@@ -3,12 +3,13 @@ from pydantic import BaseModel
 
 class State(TypedDict):
     competitors_names: List[str]
-    competitors_content: List[str]
     csv_file: str
     feedback: str
     financial_analysis: str
     financial_data: str
+    full_content: List[str]
     max_revisions: int
+    performance_comparison: str
     report: str
     revisions: int
     task: str
