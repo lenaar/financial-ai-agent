592199a6c6ca141a56d051c8bba2e19a7af1a0dc
Elena <rahimova.elena@gmail.com>
2025-06-17 15:02:29 +0200

    feat: stream events with graph agent in main

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from model import generate_model_response
 from state import State
 from prompts import GATHER_FINANCIAL_PROMPT, ANALYZE_DATA_PROMPT
 from agent import build_graph_agent
+from memory import get_memory_saver, get_thread_config
 
 load_dotenv()
 
@@ -15,21 +16,28 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 model = ChatOpenAI(api_key=openai_api_key, model="gpt-4.1-nano", temperature=0.0)
 
 def main():
-    graph = build_graph_agent()
-    result = graph.invoke({
-        "competitors_names": [],
-        "competitors_content": [],
-        "csv_file": "data/financial_data.csv",
-        "feedback": "",
-        "financial_analysis": "",
-        "financial_data": "",
-        "max_revisions": 3,
-        "report": "",
-        "revisions": 0,
-        "task": "analyze financial data",
-    })
-    print("Final result:", result)
-    return graph
+    with get_memory_saver() as checkpointer:
+        graph = build_graph_agent(checkpointer)
+        initial_state = {
+            "competitors_names": ["Apple", "Google", "Microsoft", "Amazon", "Tesla"],
+            "csv_file": "data/financial_data.csv",
+            "max_revisions": 3,
+            "revisions": 1,
+            "task": "analyze financial data and performance of the given company by reading the financial data and compare it with the competitors",
+        }
+        thread_config = get_thread_config()
+
+        try:
+            events = graph.stream(initial_state, thread_config)
+
+            for event in events:
+                print(event)
+                print("--------------------------------")
+
+        except Exception as e:
+            print(f"Error in stream processing: {e}")
+
+        return
 
 if __name__ == "__main__":
     main()
