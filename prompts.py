7212fb923032335605b11cdd021fc068c72c5f94
Elena <rahimova.elena@gmail.com>
2025-06-17 15:01:31 +0200

    feat: add and improve prompts

--- prompts.py
+++ prompts.py
@@ -1,11 +1,23 @@
-GATHER_FINANCIAL_PROMPT = """You are a financial analyst. You are given a finacials for 5 years of a company and you need to gather the financial data for a company."""
-ANALYZE_DATA_PROMPT = """You are a financial analyst. You are given a financials of a company and you need to analyze the financial data for a company."""
-ANALYZE_COMPETITORS_PROMPT = """You are a financial analyst. 
-    You are given a company and you need to research the competitors for performance comparison. 
-    Provide max 3 queries for each competitor.
-    Return the queries in a list of strings."""
-COMPARE_PERFORMANCE_PROMPT = """You are a financial analyst. You are given a list of companies and you need to compare the performance of the companies.
-    ***MAKE SURE TO COMPARE THE COMPETITORS PERFORMANCE TO THE COMPANY YOU ARE ANALYZING AND INCLUDE THE NAME OF COMPETITORS IN THE REPORT***"""
+GATHER_FINANCIAL_PROMPT = """You are a financial analyst. \n
+    You are given a finacials for 5 years of the given company and you need to gather the detailedfinancial data for a company. \n\n"""
+ANALYZE_DATA_PROMPT = """You are a financial analyst. \n
+    You are given a financials of a company and you need to analyze the financial data for a company. \n\n"""
+ANALYZE_COMPETITORS_PROMPT = """You are a financial analyst and researcher. \n
+    You are given a company and you need to research the competitors for performance comparison. \n
+    Generate a list of search queries to gather relevant information about the competitors. \n
+    Provide max 3 queries for each competitor. \n\n"""
+COMPARE_PERFORMANCE_PROMPT = """You are a financial analyst. \n
+    You are given a list of companies and you need to compare the performance of the companies. \n
+    **MAKE SURE TO COMPARE THE COMPETITORS PERFORMANCE TO THE COMPANY YOU ARE ANALYZING AND INCLUDE THE NAME OF COMPETITORS IN THE REPORT** \n\n"""
+FEEDBACK_PROMPT = """You are a financial reviewer. \n
+    Provide detailed feedback and critique for the provided financial comparison report. \n
+    Include any additional information or revisions needed. \n\n"""
+WRITE_REPORT_PROMPT = """You are a financial analyst and report writer. \n
+    You need to write a financial report based on finansial analysis, competitors analysis, comparison
+    on the performance of the companies and feedback. \n\n"""
+RESEARCH_FEEDBACK_PROMPT = """You are a financial researcher. \n
+    You are given a feedback and you need to address the feedback or the critique. \n
+    Generate a list of search queries to gather relevant information. Generate max 3 queries. \n\n"""
 
 
 
