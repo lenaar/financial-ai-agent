d5e0d4e133f8f242d3678f64fc3be5e840120e44
Elena <rahimova.elena@gmail.com>
2025-06-17 15:00:50 +0200

    feat: add additional nodes to research feedback and write report

--- nodes.py
+++ nodes.py
@@ -1,6 +1,6 @@
 import prompts 
 from langchain_core.messages import SystemMessage, HumanMessage
-from model import model, generate_model_response
+from model import model, generate_model_response, generate_model_response_with_structured_output
 from typing import Dict, List
 from state import State, Queries
 from search_tool import tavily
@@ -15,11 +15,11 @@ def gather_financial_data_node(state: State) -> State:
 def analyze_financial_data_node(state: State) -> State:
     analysis = generate_model_response(state["financial_data"], prompts.ANALYZE_DATA_PROMPT)
     print(f"Financial Analysis: {analysis}")
-    return {"analysis": analysis}
+    return {"financial_analysis": analysis}
 
 def analyze_competitors_node(state: State) -> State:
-    competitors_content = []
-    for competitor in state.get("competitors", []):
+    competitors_content = state.get("full_content") or []
+    for competitor in state.get("competitors_names", []):
         queries = model.with_structured_output(Queries).invoke(
             [
                 SystemMessage(content=prompts.ANALYZE_COMPETITORS_PROMPT),
@@ -33,13 +33,38 @@ def analyze_competitors_node(state: State) -> State:
                 competitors_content.append(result["content"])
 
     print(f"Competitors content: {competitors_content}")
-    return {"competitors_content": competitors_content}
+    return {"full_content": competitors_content}
 
 def compare_performance_node(state: State) -> State:
-    message = f"The task is {state['task']} \n\n The financial analysis is {state['analysis']} \n\n The content is {state['financial_data']}"
-    comparison_content = generate_model_response(message, prompts.COMPARE_PERFORMANCE_PROMPT)
+    message = f"The task is {state['task']} \n\n The financial analysis is {state['financial_analysis']} \n\n The content is {state['financial_data']}"
+    performance_comparison = generate_model_response(message, prompts.COMPARE_PERFORMANCE_PROMPT)
     revisions = state.get("revisions", 1) + 1
     return {
-        "comparison_content": comparison_content,
+        "performance_comparison": performance_comparison,
         "revisions": revisions,
     }
+
+def collect_feedback_node(state: State) -> State:
+    feedback = generate_model_response(state["performance_comparison"], prompts.FEEDBACK_PROMPT)
+    return {"feedback": feedback}
+
+def write_report_node(state: State) -> State:
+    report = generate_model_response(state["performance_comparison"], prompts.WRITE_REPORT_PROMPT)
+    return {"report": report}
+
+def research_feedback_node(state: State) -> State:
+    queries = model.with_structured_output(Queries).invoke(
+        [
+            SystemMessage(content=prompts.RESEARCH_FEEDBACK_PROMPT),
+            HumanMessage(content=state["feedback"])
+        ]
+    )
+
+    content = state["full_content"] or []
+
+    for query in queries.queries:
+        response = tavily.search(query=query, max_results=2)
+        for result in response["results"]:
+            content.append(result["content"])
+    return {"full_content": content}
+
